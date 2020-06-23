13b53cedef9f24c7f7d492aaf754c19bc525aadf
Yahui Yang <52389598+yangyahuiOnline@users.noreply.github.com>
2020-06-24 00:57:39 +0800

    chore(*):考虑挂载到react原型上

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import List from "./list";
 export default function App() {
   const a = 24;
   /**
-   * 全局挂载lodash
+   * 方式1:全局挂载lodash
+   * 方式2：全局挂载在react对象上
+   * 思考有没有更加优雅的全局挂载方式
    */
   window._ = _;
   console.log(window);
